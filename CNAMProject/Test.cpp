6fcde8b5f5f35e916a4b19035825997ac9f1a3cd
samielatri <samielatri.pro@gmail.com>
2021-06-06 22:35:09 +0200

    Add project files.

---
+++ CNAMProject/Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+	return 0;
+
+}

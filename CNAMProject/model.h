c5937dd43b543586da382c1c3e03779385c65ff3
Sami ELATRI <samielatri.pro@gmail.com>
2021-06-13 01:07:41 +0200

    Create model.h

---
+++ CNAMProject/model.h
@@ -0,0 +1,12 @@
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#ifdef __unix__         
+  #define FACE_DEFINITION "Resources/haarcascade_frontalface_default.xml"
+	#define OUTPUT_FILE "output/output.jpg"
+#elif defined(_WIN32) || defined(WIN32) 
+	#define FACE_DEFINITION "Resources\\haarcascade_frontalface_default.xml"
+	#define OUTPUT_FILE "output\\output.jpg"
+#endif /* __unix__ */
+
+#endif  /* _MODEL_H_ */

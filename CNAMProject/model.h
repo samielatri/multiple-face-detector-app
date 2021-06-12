14d056bab260160bcb18cc19705bce2e0a3505d8
Sami ELATRI <samielatri.pro@gmail.com>
2021-06-13 01:32:12 +0200

    Update model.h

--- CNAMProject/model.h
+++ CNAMProject/model.h
@@ -2,10 +2,10 @@
 #define _MODEL_H_
 
 #ifdef __unix__         
-  #define FACE_DEFINITION "Resources/haarcascade_frontalface_default.xml"
+  #define FACE_DEFINITION "Resources/model/haarcascade_frontalface_default.xml"
 	#define OUTPUT_FILE "output/output.jpg"
 #elif defined(_WIN32) || defined(WIN32) 
-	#define FACE_DEFINITION "Resources\\haarcascade_frontalface_default.xml"
+	#define FACE_DEFINITION "Resources\\model\\haarcascade_frontalface_default.xml"
 	#define OUTPUT_FILE "output\\output.jpg"
 #endif /* __unix__ */
 
